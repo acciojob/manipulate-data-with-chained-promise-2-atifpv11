508aa3f2c7c3cf1d0856b71d70a7d6212c198438
atifpv11 <115897151+atifpv11@users.noreply.github.com>
2024-10-16 22:57:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+async function getArray(){
+	let array = [1,2,3,4];
+	return array;
+}
+
+getArray().then((array)=>{
+	let evens = array.filter((i)=>i%2===0);
+	setTimeout(()=>{
+		let element = document.getElementById("output");
+		element.innerHTML = evens.join(',');
+	},1000)
+	return evens;
+}).then((evens)=>{
+	let twos = evens.map((i)=>i*2);
+	setTimeout(()=>{
+		let element = document.getElementById("output");
+		element.innerHTML = twos.join(',');
+	},2000)
+})
